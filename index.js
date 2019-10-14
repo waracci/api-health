1641fb6ac729d390dc506e79e35e4c780da3d245
warachi <morriswarachi0@gmail.com>
2019-10-14 18:26:54 +0300

    feat(initial): initial commit
    - Initial setup of the middleware
    
    [Finishes setup]

---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+module.exports = function (req, res, next){
+  var urlLogData = {
+    accepts: req.accepts(['html', 'json', 'text/html', 'text', 'application/json', 'image/png']),
+    url: req.url,
+    method: req.method,
+    date: Date(),
+    time: Date.now(),
+    protocol: req.protocol,
+    secure: req.secure,
+    hostname: req.hostname,
+    path: req.path,
+    originalUrl: req.originalUrl,
+    subdomain: req.subdomain,
+    contentType: req.header('Content-Type'),
+    requestHeader: req.header('user-agent'),
+    cookies: req.cookies,
+    signedCookies: req.signedCookies,
+    baseUrl: req.baseUrl,
+    fresh: req.fresh,
+    ip: req.ip,
+    params: req.params,
+    query: req.query,
+    route: req.route,
+    xhr: req.xhr,
+  };
+  axios.post(process.env.API_HEALTH_API_KEY, urlLogData)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error.data);
+  });
+  next()
+}
